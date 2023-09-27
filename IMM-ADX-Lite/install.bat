2d9aac2253275a4e634e24af44467bf72b0856ba
mhlee0328 <mhlee0911779731@gmail.com>
2023-09-27 10:32:13 +0800

    fix: 修正IMM-ADX-Lite.png 與 ArmDot授權7天版本

--- IMM-ADX-Lite/install.bat
+++ IMM-ADX-Lite/install.bat
@@ -3,7 +3,11 @@ chcp 65001
 echo.
 rem 確認 Docker Engine Daemon 運行後再執行以下動作
 echo 正在確認 Docker Desktop CE 服務運行狀態中  . . .
-docker info > nul 2>&1
+
+rem 以下兩行給 Win11預覽版 WSL Disable 需要排除 ERROR: error during connect: Get "http://host.docker.internal:2375/v1.24/info": dial tcp 172.20.10.2:2375: connectex: No connection could be made because the target machine actively refused it.
+rem Remove-Item Env:\DOCKER_HOST 
+rem $env:DOCKER_HOST="tcp://localhost:2375"
+docker info | Out-Null
 if %errorlevel% neq 0 (
     echo.
     echo.
@@ -26,7 +30,7 @@ if %errorlevel% neq 0 (
 
 rem 過濾 Name: docker-desktop
 echo 確認 Docker Desktop CE 服務運行狀態中  . . .
-docker info | findstr /C:"Name: docker-desktop" > nul 2>&1  
+docker info | findstr /C:"Name: docker-desktop" | Out-Null
 if %errorlevel% neq 0 (
     echo.
     echo.
